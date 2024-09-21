64ab4046d87ee2bed23bca07b21219ccd3e648d8
Lucas Petzinger <lucaswinpetzinger@gmail.com>
2024-09-21 14:23:18 -0300

    feature: repository creation

--- app/src/main/java/com/petzinger/magalu/network/GitHubApi.kt
+++ app/src/main/java/com/petzinger/magalu/network/GitHubApi.kt
@@ -2,6 +2,7 @@ package com.petzinger.magalu.network
 
 import com.petzinger.magalu.model.PullRequest
 import com.petzinger.magalu.model.RepositoryResponse
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -12,11 +13,11 @@ interface GitHubApi {
         @Query("q") language: String,
         @Query("sort") sort: String,
         @Query("page") page: Int
-    ): RepositoryResponse
+    ): Single<RepositoryResponse>
 
     @GET("repos/{owner}/{repo}/pulls")
     fun getPullRequests(
         @Path("owner") owner: String,
         @Path("repo") repo: String
-    ): List<PullRequest>
+    ): Single<List<PullRequest>>
 }
