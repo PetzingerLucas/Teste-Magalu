ed26c30e623ab37cb27358c51e42671608c08ff9
Lucas Petzinger <lucaswinpetzinger@gmail.com>
2024-09-22 22:42:11 -0300

    fix: fix vm unit tests

--- app/src/test/java/com/petzinger/magalu/ui/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/petzinger/magalu/ui/viewmodel/MainViewModelTest.kt
@@ -1,10 +1,10 @@
 package com.petzinger.magalu.ui.viewmodel
 
-
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.petzinger.magalu.data.DataSource
-import com.petzinger.magalu.data.DataSourceImpl
+import com.petzinger.magalu.model.pullrequest.PullRequest
+import com.petzinger.magalu.model.pullrequest.User
 import com.petzinger.magalu.model.repository.RepositoryResponse
 import com.petzinger.magalu.ui.repository.RepositoryIntent
 import com.petzinger.magalu.ui.repository.RepositoryState
@@ -16,7 +16,10 @@ import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.times
 
 @RunWith(MockitoJUnitRunner::class)
 class MainViewModelTest {
@@ -40,31 +43,112 @@ class MainViewModelTest {
 
     @Test
     fun `should update state to loading when fetching repositories`() {
-        // Arrange
         val repositoryResponse = RepositoryResponse("", listOf())
         Mockito.`when`(dataSource.fetchRepositories("kotlin", "stars", 1))
             .thenReturn(Single.just(repositoryResponse))
 
-        // Act
-        viewModel.processIntent(RepositoryIntent.LoadRepositories(page = 1, language = "kotlin", sort= "stars"))
+        viewModel.processIntent(
+            RepositoryIntent.LoadRepositories(
+                page = 1,
+                language = "kotlin",
+                sort = "stars"
+            )
+        )
 
-        // Assert
-        Mockito.verify(stateObserver).onChanged(RepositoryState(isLoading = true))
-        Mockito.verify(stateObserver).onChanged(RepositoryState(isLoading = false, repositories = repositoryResponse.items))
+        val captor = argumentCaptor<RepositoryState>()
+        verify(stateObserver, times(3)).onChanged(captor.capture())
+
+        val capturedStates = captor.allValues
+
+        val firstState = capturedStates[0]
+        assert(!firstState.isLoading) { "Expected isLoading to be false" }
+
+        val secondState = capturedStates[1]
+        assert(secondState.isLoading) { "Expected isLoading to be true" }
+        assert(secondState.repositories == repositoryResponse.items) { "Expected repositories to match" }
+
+        val thirdState = capturedStates[2]
+        assert(!thirdState.isLoading)
     }
 
     @Test
     fun `should update state to error when fetching repositories fails`() {
-        // Arrange
         val errorMessage = "Network error"
         Mockito.`when`(dataSource.fetchRepositories("kotlin", "stars", 1))
             .thenReturn(Single.error(Throwable(errorMessage)))
 
-        // Act
-        viewModel.processIntent(RepositoryIntent.LoadRepositories(page = 1, language = "kotlin", sort= "stars"))
+        viewModel.processIntent(
+            RepositoryIntent.LoadRepositories(
+                page = 1,
+                language = "kotlin",
+                sort = "stars"
+            )
+        )
 
-        // Assert
-        Mockito.verify(stateObserver).onChanged(RepositoryState(isLoading = true))
-        Mockito.verify(stateObserver).onChanged(RepositoryState(isLoading = false, error = errorMessage))
+        verify(stateObserver).onChanged(RepositoryState(isLoading = true))
+        verify(stateObserver).onChanged(RepositoryState(isLoading = false, error = errorMessage))
+    }
+
+    @Test
+    fun `should update state to loading when fetching pull requests`() {
+        val pullRequests = listOf(
+            PullRequest(
+                id = 1,
+                title = "PR 1",
+                body = "test body",
+                createdAt = "test2",
+                user = User("Teste", "url.com"),
+                url = "http://example.com/1"
+            ),
+            PullRequest(
+                id = 2,
+                title = "PR 2",
+                body = "test body 2",
+                createdAt = "test",
+                user = User("Teste2", "url2.com"),
+                url = "http://example.com/2"
+            )
+        )
+        Mockito.`when`(dataSource.fetchPullRequests("owner", "repo"))
+            .thenReturn(Single.just(pullRequests))
+
+        viewModel.processIntent(
+            RepositoryIntent.LoadPullRequests(
+                owner = "owner",
+                repo = "repo"
+            )
+        )
+
+        val captor = argumentCaptor<RepositoryState>()
+        verify(stateObserver, times(3)).onChanged(captor.capture())
+
+        val capturedStates = captor.allValues
+
+        val firstState = capturedStates[0]
+        assert(!firstState.isLoading) { "Expected isLoading to be false" }
+
+        val secondState = capturedStates[1]
+        assert(secondState.isLoading) { "Expected isLoading to be true" }
+
+        val thirdState = capturedStates[2]
+        assert(!thirdState.isLoading) { "Expected isLoading to be false" }
+        assert(thirdState.pullRequests == pullRequests) { "Expected pullRequests to match" }
+    }
+
+    @Test
+    fun `should update state to error when fetching pull requests fails`() {
+        val errorMessage = "Network error"
+        Mockito.`when`(dataSource.fetchPullRequests("owner", "repo"))
+            .thenReturn(Single.error(Throwable(errorMessage)))
+
+        viewModel.processIntent(
+            RepositoryIntent.LoadPullRequests(
+                owner = "owner",
+                repo = "repo"
+            )
+        )
+
+        verify(stateObserver).onChanged(RepositoryState(isLoading = true))
+        verify(stateObserver).onChanged(RepositoryState(isLoading = false, error = errorMessage))
     }
 }
